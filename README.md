3893773af5499e8217f56ebd6b23580ba307051b
valudollar <valerie.luxuanhui@gmail.com>
2023-06-26 22:32:54 +0800

    painting completed and updated readme

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app)and deployed on [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
-## Getting Started
+I reconstructed Piet Mondrian's Composition with Red, Blue and Yellow (1930) with CSS!
+It has been scaled to 450x450px with respect to the painting's original dimensions of 45 x 45 cm.
 
-First, run the development server:
+<p align="center">
+  <img src="./painting.jpeg" width="350" ">
+  <p align="center">Image Source: Wikipedia</p>
+</p>
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-```
+Something interesting during this fun exercise was that when I first thought I had completed it, I seemed to be about one pixel off and after on checking back to the original painting, I found that there was this detail I had missed where Mondrian did not paint the black line in the bottom right corner to the edge of the canvas.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+<p align="center">
+  <img src="./corner.png" width="350" ">
+   <p align="center">Image Source: Wikipedia</p>
+</p>
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+And of course I had to recreate it as well to pay homage to Mondrian and his work!
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+<p align="center">
+  <img src="./final.gif" width="350" ">
+    <p align="center">A GIF of the step-by-step reconstruction process!</p>
+</p>
--- app/page.js
+++ app/page.js
@@ -1,95 +1,46 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <head>
+        <title>Composition With Red, Blue and Yellow</title>
+      </head>
+      <body>
+        <div className={styles.MainContainer}>
+          <div className={styles.Canvas}>
+            <div className={styles.ColOne}>
+              <div className={styles.OneA}></div>
+              <div className={styles.OneB}></div>
+              <div className={styles.OneC}></div>
+              <div className={styles.OneD}></div>
+              <div className={styles.OneE}></div>
+            </div>
+            <div className={styles.ColTwo}></div>
+            <div className={styles.ColThree}>
+              <div className={styles.ThreeA}></div>
+              <div className={styles.ThreeB}></div>
+              <div className={styles.ThreeC}>
+                <div className={styles.ThreeCOne}></div>
+                <div className={styles.ThreeCTwo}></div>
+                <div className={styles.ThreeCThree}>
+                  <div className={styles.ThreeCThreeA}></div>
+                  <div className={styles.ThreeCThreeB}>
+                    <div className={styles.ThreeCThreeBOne}></div>
+                    <div className={styles.ThreeCThreeBTwo}>
+                      <div className={styles.ThreeCThreeBTwoA}></div>
+                      <div className={styles.ThreeCThreeBTwoB}></div>
+                    </div>
+                  </div>
+                  <div className={styles.ThreeCThreeC}></div>
+                  <div className={styles.ThreeCThreeD}></div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </body>
+    </>
+  );
 }
